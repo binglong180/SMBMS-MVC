b177b33705cba9b4066dff3c6c1fa0c11fa581a5
binglong180 <907555993@qq.com>
2018-02-03 11:46:54 +0800

    编写自定义转换器日期转换器

--- src/cn/smbms/pojo/User.java
+++ src/cn/smbms/pojo/User.java
@@ -21,8 +21,6 @@ public class User {
 	private String userPassword; // 用户密码
 	private Integer gender; // 性别
 	@Past(message = "必须是一个过去的时间")
-	
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date birthday; // 出生日期
 	private String phone; // 电话
 	private String address; // 地址
